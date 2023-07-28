5831602a2110bbc19ca41bd8b83c0f1769da5560
Srora Mensah <mensahsrora36@gmail.com>
2023-07-28 17:36:45 +0000

    nav

---
+++ src/component/nav/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { logo } from '../../assets'
+
+function Nav() {
+  return (
+      <header>
+          <img src={logo} />
+          <nav>
+              <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Menu</li>
+                  <li>Reservations</li>
+                  <li>Reservations</li>
+                  <li>Order online</li>
+                  <li>Order online</li>
+                  <li>Login</li>
+              </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
