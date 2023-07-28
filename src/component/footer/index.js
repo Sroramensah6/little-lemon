ee7255c9e8d4ef180337dcd02199d935db7735fc
Srora Mensah <mensahsrora36@gmail.com>
2023-07-28 17:36:25 +0000

    Footer

---
+++ src/component/footer/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <section>
+                <h5>Doormat Navigation</h5>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+            </section>
+            <section>
+                <h5>Contact</h5>
+                <ul>
+                    <li>Adress</li>
+                    <li>Phone number</li>
+                    <li>Email</li>
+                </ul>
+            </section>
+            <section>
+                <h5>Social Media Links</h5>
+                <ul>
+                    <li>Adress</li>
+                    <li>Phone number</li>
+                    <li>Email</li>
+                </ul>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
