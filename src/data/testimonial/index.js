2271f5e5710d61f8f4c8a8d429e75e822e9f4643
Srora Mensah <mensahsrora36@gmail.com>
2023-07-31 15:35:23 +0000

    testimonial_data

---
+++ src/data/testimonial/index.js
@@ -0,0 +1,32 @@
+const testimonial_data = [
+    {
+        id:'1',
+        stars: 5,
+        name: 'Bubu Mensah',
+        testimony: 'We are a family owned Mediterranean restaurant, focused on traditional recipes',
+        img: "https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg",
+    },
+    {
+        id:'2',
+        stars: 4,
+        name: 'Tina Mensah',
+        testimony: 'We are a family owned Mediterranean restaurant, focused on traditional recipes',
+        img: "https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg",
+    },
+    {
+        id:'3',
+        stars: 4,
+        name: 'Jane Doe',
+        testimony: 'We are a family owned Mediterranean restaurant, focused on traditional recipes',
+        img: "https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg",
+    },
+    {
+        id:'4',
+        stars: 4,
+        name: 'John Doe',
+        testimony: 'We are a family owned Mediterranean restaurant, focused on traditional recipes',
+        img: "https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg",
+    }
+]
+
+export default testimonial_data
