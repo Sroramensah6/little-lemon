eb56b83d2a4a22d0e0cec48f425d0d5da7e398e3
Srora Mensah <mensahsrora36@gmail.com>
2023-07-31 15:35:11 +0000

    food

---
+++ src/data/food/index.js
@@ -0,0 +1,27 @@
+import { bruchetta, greek_salad, lemon_dessert } from "../../assets"
+
+const food_data = [
+    {
+        id: 1,
+        price: '$ 5.99',
+        image: greek_salad,
+        title: 'Greek salad',
+        description: 'Greek We are a family owned Mediterranean restaurant, focused on traditional recipes'
+    },
+    {
+        id : 2,
+        price: '$ 15.99',
+        image: lemon_dessert,
+        title: 'Lemon dessert',
+        description: 'Lemon dessert are a family owned Mediterranean restaurant, focused on traditional'
+    },
+    {
+        id : 3,
+        price: '$ 2.99',
+        image: bruchetta,
+        title: 'Bruchetta',
+        description: 'Bruchetta We are a family owned Mediterranean restaurant, focused on traditional recipes'
+    },
+]
+
+export default food_data
