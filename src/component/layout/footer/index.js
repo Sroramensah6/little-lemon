5fa743c6ed95c390bd81e93c67162b386596b4bc
Srora Mensah <mensahsrora36@gmail.com>
2023-07-31 15:32:36 +0000

    footer

---
+++ src/component/layout/footer/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Footer() {
+    return (
+        <footer id="footer" className="container">
+            <section>
+                <h5>Doormat Navigation</h5>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="#">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            menu
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Reservations
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Order Online
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Login
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h5>Contact</h5>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Address
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Phone number
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Email
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h5>Social Media Links</h5>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Address
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Phone number
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Email
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
