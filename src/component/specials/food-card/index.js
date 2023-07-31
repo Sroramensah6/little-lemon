a6d2dd5b9c3b0e618e3ac1e3ab691b1f022ab8cb
Srora Mensah <mensahsrora36@gmail.com>
2023-07-31 15:34:33 +0000

    FoodCard

---
+++ src/component/specials/food-card/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Card from '../../card'
+import Image from '../../image'
+
+function FoodCard({ title, price, content, img_src }) {
+    return (
+        <Card className={'food-card'}>
+            {img_src ? (
+                <div className='img-wrapper'>
+                    <Image src={img_src} className='card-img' alt="food" />
+                </div>
+            ) : null}
+            <div>
+                <header className='card-header'>
+                    {title ? <h3>{title}</h3>: null}
+                    {price ? <h5>{price}</h5> : null}
+                </header>
+                {content ? (
+                    <article className='card-body'>
+                        <p>{content}</p>
+                    </article>
+                ) : null}
+            </div>
+        </Card>
+    )
+}
+
+export default FoodCard
