65bed37466dc72add859cf5e3aac221688435eb2
Srora Mensah <mensahsrora36@gmail.com>
2023-07-31 15:36:33 +0000

    home

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { About, Hero, Specials, Testimonial } from '../../component'
+
+function Home() {
+    return (
+        <>
+            <Hero />
+            <Specials />
+            <Testimonial />
+            <About />
+        </>
+    )
+}
+
+export default Home
